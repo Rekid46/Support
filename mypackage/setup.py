85e360447dfa4624445045884e2ce9553b793b20
Ranjan Singh <singhranjan9024@gmail.com>
2024-06-26 11:55:09 +0530

    Update setup.py

--- mypackage/setup.py
+++ mypackage/setup.py
@@ -14,7 +14,7 @@ setup(
 
     description="A simple example package",
 
-    long_description=open("README.md").read(),
+    long_description=open("./README.md").read(),
 
     long_description_content_type="text/markdown",
 
