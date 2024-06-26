5200498d62305ef6968aacac8cd2ed79dc5c5281
Ranjan Singh <singhranjan9024@gmail.com>
2024-06-26 11:17:27 +0530

    Create setup.py

---
+++ mypackage/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name="mypackage",
+
+    version="0.1.0",
+
+    packages=find_packages(),
+
+    author="Ranjn Singh",
+
+    author_email="singhranjan9024@gmail.com",
+
+    description="A simple example package",
+
+    long_description=open("README.md").read(),
+
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/Rekid46/mypackage",
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: OS Independent",
+
+    ],
+
+    python_requires='>=3.6',
+
+)
